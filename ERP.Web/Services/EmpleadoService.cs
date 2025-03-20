3b1a52203fd4222c9933c75a85917e4ac906de7f
Sr-Smith-488 <2019-0483@uteco.edu.do>
2025-03-20 16:19:31 -0400

    Se le aádió el C.R.U.D empleados a la app web ERP

---
+++ ERP.Web/Services/EmpleadoService.cs
@@ -0,0 +1,85 @@
+﻿using ERP.Web.Data;
+using ERP.Web.Domain.Dto;
+using ERP.Web.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+namespace ERP.Web.Services
+{
+	public interface IEmpleadoService
+	{
+		Task<List<EmpleadoDto>> Consultar(string filtro);
+		Task<bool> Crear(EmpleadoDto request);
+		Task<bool> Eliminar(int Id);
+		Task<bool> Modificar(EmpleadoDto request);
+	}
+
+	public class EmpleadoService : IEmpleadoService
+	{
+		private readonly AppDbContext _context;
+		public EmpleadoService(AppDbContext context)
+		{
+			_context = context;
+		}
+		///Consultar los empleados existentes
+		///
+		public async Task<List<EmpleadoDto>> Consultar(string filtro)
+		{
+			var empleados = await
+				_context.Empleados
+				.Include(c => c.DatosPersonales)
+				.Where(c => c.DatosPersonales.Nombre.Contains(filtro))
+				.Select(
+					c =>
+					new EmpleadoDto()
+					{
+						Id = c.Id,
+						PersonaId = c.PersonaId,
+						Sueldo = c.Sueldo,
+						DatosPersonales = new PersonaDto()
+						{
+							Id = c.DatosPersonales.Id,
+							Nombre = c.DatosPersonales.Nombre,
+							FechaDeNacimiento = c.DatosPersonales.FechaDeNacimiento
+						}
+					}
+				)
+				.ToListAsync();
+			return empleados;
+		}
+
+		public async Task<bool> Crear(EmpleadoDto request)
+		{
+			var empleado = Empleado.Create(
+				request.DatosPersonales.Nombre,
+				request.DatosPersonales.FechaDeNacimiento,
+				request.Sueldo
+			);
+			_context.Empleados.Add(empleado);
+			;
+			return (await _context.SaveChangesAsync()) > 0;
+		}
+
+		public async Task<bool> Modificar(EmpleadoDto request)
+		{
+			//1. Busco el Empleado
+			var empleado = await _context.Empleados
+				.Include(c => c.DatosPersonales)
+				.FirstOrDefaultAsync(c => c.Id == request.Id);
+			//2. Modifico el cliente
+			empleado!.DatosPersonales.Nombre = request.DatosPersonales.Nombre;
+			empleado!.DatosPersonales.FechaDeNacimiento = request.DatosPersonales.FechaDeNacimiento;
+			empleado!.Sueldo = request.Sueldo;
+			//Guardo los cambios
+			return (await _context.SaveChangesAsync()) > 0;
+		}
+
+		public async Task<bool> Eliminar(int Id)
+		{
+			var empleado = await _context.Empleados
+				.FirstOrDefaultAsync(c => c.Id == Id);
+
+			_context.Empleados.Remove(empleado!);
+
+			return (await _context.SaveChangesAsync()) > 0;
+		}
+	}
+}
